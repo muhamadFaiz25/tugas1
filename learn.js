31f59427c1efec35de75f8793d92ee7e62ddf794
Muhamad Faiz <mfaiz2502@gmail.com>
2021-12-30 12:58:42 +0700

    Latihan 5.4: Melakukan operasi sederhana dengan operator logika

--- learn.js
+++ learn.js
@@ -31,19 +31,33 @@
 // console.log('x-- =', x--)
 // console.log('--x =', --x)
 
-// Latihan 5.3. Melakukan operasi sederhana dengan operator perbandingan
+// // Latihan 5.3. Melakukan operasi sederhana dengan operator perbandingan
 
-// membuat variabel
-let a = 28
-let b = 30
+// // membuat variabel
+// let a = 28
+// let b = 30
+
+// // menampilkan hasil ke console
+// console.log(a, b)
+// console.log('a > b =', a > b)
+// console.log('a < b =', a < b)
+// console.log('a >= b =', a >= b)
+// console.log('a <= b =', a <= b)
+// console.log('a == b =', a == b)
+// console.log('a != b =', a != b)
+// console.log('a === b =', a === b)
+// console.log('a !== b =', a !== b)
+
+// Latihan 5.4. Melakukan operasi sederhana dengan operator logika
 
 // menampilkan hasil ke console
-console.log(a, b)
-console.log('a > b =', a > b)
-console.log('a < b =', a < b)
-console.log('a >= b =', a >= b)
-console.log('a <= b =', a <= b)
-console.log('a == b =', a == b)
-console.log('a != b =', a != b)
-console.log('a === b =', a === b)
-console.log('a !== b =', a !== b)
+console.log('true && true =', true && true)
+console.log('true && false =', true && false)
+console.log('false && true =', false && true)
+console.log('false && false =', false && false)
+console.log('true || true =', true || true)
+console.log('true || false =', true || false)
+console.log('false || true =', false || true)
+console.log('false || false =', false || false)
+console.log('!true =', !true)
+console.log('!false =', !false)
